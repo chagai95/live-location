1bb24ce0b5305b70c1554abc59400d066e611141
chagai.friedlander <chagai.friedlander@fairkom.eu>
2024-03-23 13:36:04 +0100

    make it easier to add new cookie files based on their txt file ending

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # live-location
+
+My docker run command:
+docker run -d --network traefik --restart always -v /root/location/www.google.com_cookies.txt:/www.google.com_cookies.txt -v /matrix/traefik-certs-dumper/dumped-certificates/api.chagai.website/certificate.crt:/certificate.crt -v /matrix/traefik-certs-dumper/dumped-certificates/api.chagai.website/privatekey.key:/privatekey.key -v /matrix/chagai-location/config/config.toml:/config/config.toml --env-file /matrix/chagai-location/env --label-file /matrix/chagai-location/labels --name chagai-location -p 8080:4400 chagai/chagai-location:1.0.5
--- location.py
+++ location.py
@@ -8,15 +8,30 @@ from locationsharinglib import Service
 from flask import Flask
 from flask_cors import CORS
 
+import os
+
+from os import walk
+
+filenames = next(walk("./"), (None, None, []))[2]  # [] if no file
+
+#print all the file names
+for name in filenames:
+    try:
+        if name.split(".")[-1] == "txt" and not name == "requirements.txt":
+            cookies_file_name = name
+    except:
+        pass
+print(cookies_file_name)
+
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*"}})
 
 @app.route("/location", methods=["GET"])
 def translate():
     
-    cookies_file = 'www.google.com_cookies.txt'
+    cookies_file_name = 'www.google.com_cookies.txt'
     google_email = 'chagai95@gmail.com'
-    service = Service(cookies_file=cookies_file, authenticating_account=google_email)
+    service = Service(cookies_file=cookies_file_name, authenticating_account=google_email)
     nickname = "Chagai"
     person = service.get_person_by_nickname(nickname)
     return json.dumps({"accuracy": person.accuracy, "address": person.address, "battery_level": person.battery_level, "charging": person.charging, "latitude": person.latitude, "longitude": person.longitude, "timestamp": person.timestamp}).encode("ascii"), 200
